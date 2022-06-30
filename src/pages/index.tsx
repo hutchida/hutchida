6df1ad81d3b0360187a360faf653f91175ca39dd
Dan <danielmhutchings@gmail.com>
2022-06-30 09:20:13 +0200

    configure basic storyblok pagedata getter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,50 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Section, EventsAccordion } from 'knit-hutchida/lib'
+import { Section, EventsAccordion, Slide } from 'knit-hutchida/lib'
 import data from '../lib/cv.json'
+import StoryblokClient from 'storyblok-js-client'
+import { GetStaticProps } from "next";
 
-const Home: NextPage = () => {
+const Storyblok = new StoryblokClient({
+  accessToken: '1JIP9C8i6yPABNQVN9aWIwtt',
+  cache: {
+    clear: 'auto',
+    type: 'memory'
+  }
+})
+
+
+/**
+ * When the user navigates to the root of the site this page will be delivered. It is hardcoded
+ * to fetch data for a page called 'homepage' in the cms
+ * @returns
+ */
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  async function getPageData() {
+    let data;
+    try {
+      data = await Storyblok.get('cdn/stories/vue-js-conference-2022?cv=1595529854', {})
+    } catch (error: any) {
+      console.log(error);
+    }
+    return data?.data?.story?.content
+  }
+
+  const pageData = await getPageData();
+  console.log('pageData', pageData)
+
+  return {
+    props: {
+      pageData: pageData || null,
+    },
+  };
+}
+
+const Home = (props: any) => {
+  console.log('props', props)
+  const slideData = props?.pageData?.body[0]
   return (
     <div className={styles.container}>
       <Head>
@@ -17,6 +57,18 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           HUTCHIDA
         </h1>
+        <Section>
+          <Slide
+            title={slideData.title}
+            description={slideData.description}
+            descriptionSize={slideData.descriptionSize}
+            bgImage={slideData.bgImage}
+            bgColor={slideData.bgColor}
+            slideWidth={slideData.slideWidth}
+            slideHeight={slideData.slideHeight}
+            spacing={slideData.spacing}
+          />
+        </Section>
         <Section className={styles.section}>
           <p className={styles.oneLiner}>{data?.oneLiner}</p>
         </Section>
