37b2587b0923ac46d137b2953b32d711e2c3534e
Dan <danielmhutchings@gmail.com>
2022-09-02 13:02:26 +0200

    fix build issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,31 +2,31 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'knit-hutchida/lib/style.css'
 import { Navigation } from 'knit-hutchida';
-// import Feature from '../components/Feature';
-// import Slide from '../components/Slide';
-// import Page from '../components/Page';
-// import { storyblokInit, storyblokEditable, apiPlugin } from '@storyblok/react';
-// const SlideContainer = (blok: any) => {
-//   console.log("blok", blok)
-//   return (
-//     <div
-//       {...storyblokEditable(blok)}
-//       key={blok._uid}>
-//       <Slide {...blok.blok}></Slide>
-//     </div>)
-// }
+import Feature from '../components/Feature';
+import Slide from '../components/Slide';
+import Page from '../components/Page';
+import { storyblokInit, storyblokEditable, apiPlugin } from '@storyblok/react';
+const SlideContainer = (blok: any) => {
+  console.log("blok", blok)
+  return (
+    <div
+      {...storyblokEditable(blok)}
+      key={blok._uid}>
+      <Slide {...blok.blok}></Slide>
+    </div>)
+}
 
-// const components = {
-//   feature: Feature,
-//   slide: SlideContainer,
-//   page: Page,
-// };
+const components = {
+  feature: Feature,
+  slide: SlideContainer,
+  page: Page,
+};
 
-// storyblokInit({
-//   accessToken: '1JIP9C8i6yPABNQVN9aWIwtt',
-//   use: [apiPlugin],
-//   components,
-// });
+storyblokInit({
+  accessToken: '1JIP9C8i6yPABNQVN9aWIwtt',
+  use: [apiPlugin],
+  components,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
